261e3d18423949f7813f1292592347cb400ee0d3
Daniela Ingratta <dingratta@frba.utn.edu.ar>
2021-04-24 23:04:41 -0300

    Agrego funciones y tests

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,41 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Horas = Number
+type Objetivo = String
+type Presion = Number
+
+presionGolpe :: Horas -> Objetivo -> Presion
+presionGolpe horas objetivo = poder horas / fortaleza objetivo
+
+poder :: Horas -> Number
+poder = (* 15)
+
+fortaleza :: Objetivo -> Number
+fortaleza = (2*) . length
+
+type Golpe = Objetivo -> Number
+
+gomuGomu :: Golpe
+gomuGomu = presionGolpe 180
+
+normalesConsecutivos :: Golpe 
+normalesConsecutivos = presionGolpe 240
+
+esobjDificil :: String -> Bool
+esobjDificil = (<100) . gomuGomu 
+
+esobjAccesible :: String -> Bool
+esobjAccesible = estaEntre . normalesConsecutivos . focalizoObj
+
+estaEntre :: Number -> Bool
+estaEntre x = 200< x && x < 400
+
+focalizoObj :: String -> String
+focalizoObj = take 7
+
+
+
+
+
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,23 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
-
+  describe "presionGolpe" $ do
+    it "Golpe de 150 horas sobre un puf debe ser 375 " $ do
+      presionGolpe 150 "puf" `shouldBe` 375
+  describe "gomuGomu" $ do
+    it "La tecnica Gomu Gomu ejerce una presion de 450 sobre el puf " $ do
+      gomuGomu "puf" `shouldBe` 450
+  describe "normalesConsecutivos" $ do
+    it "La tecnica golpes normales consecutivos ejerce una presión de 81 sobre el puf" $ do
+      normalesConsecutivos "puf" `shouldBe` 600
+  describe "esobjDificil" $ do
+    it "La bolsa de entrenamiento es un objetivo difícil " $ do
+      esobjDificil  "bolsa de entrenamiento" `shouldBe` True
+    it "El puf no es un objetivo difícil " $ do
+      esobjDificil "puf" `shouldBe` False
+  describe "esobjAccesible" $ do
+    it "Una bolsa de entrenamiento es accesible " $ do
+      esobjAccesible "bolsa de entrenamiento" `shouldBe` True
+    it "Una puf no es accesible " $ do
+      esobjAccesible "puf" `shouldBe` False
+      
